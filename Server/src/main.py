9d3b0c02dd806263aa5ad82e96c5da4a0d2906a4
juannluengo <juanluengogallego@gmail.com>
2023-10-11 15:51:40 +0200

    Not much. I got stuck with the alpha.... Will keeo from Ties code

--- Server/src/main.py
+++ Server/src/main.py
@@ -1,4 +1,6 @@
 import rpyc
+import requests
+from alpha_vantage.timeseries import TimeSeries
 
 class StockService(rpyc.Service):
 
@@ -20,6 +22,24 @@ class StockService(rpyc.Service):
 
     def get_question(self):  # while this method is not exposed
         return "what is the airspeed velocity of an unladen swallow?"
+    
+    def get_stock_price(self, symbol):
+        api_key = "RG6Z56SVLUCAVF4Q"
+        base_url = f"https://www.alphavantage.co/query?"
+
+        
+        function = "GLOBAL_QUOTE"
+        final_url = f"{base_url}function={function}&symbol={symbol}&apikey={api_key}"
+        response = requests.get(final_url)
+        if response.status_code == 200:
+            data = response.json()
+            if "Global Quote" in data and "05. price" in data["Global Quote"]:
+                return float(data["Global Quote"]["05. price"])
+            else:
+                raise ValueError(f"Unexpected data format: {data}")
+        else:
+            raise ConnectionError(f"Failed to retrieve data: {response.status_code}")
+
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
