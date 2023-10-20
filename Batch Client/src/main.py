7e61c2995704b1d093b5839835c7e74e9b68886d
ties0017 <tiestienhoven@gmail.com>
2023-10-20 12:24:00 +0200

    Updated batch client to use async requests

--- Batch Client/src/main.py
+++ Batch Client/src/main.py
@@ -7,6 +7,17 @@ def read_tickers():
     for line in file:
         tickers.append(line[:-1])
 
+def print_result(req):
+    # print(req.value)
+    pass
+
+def check_reqs(reqs):
+    for req in reqs:
+        if not req.ready:
+            return False
+    
+    return True
+
 interval = int(sys.argv[1])
 ip_address = sys.argv[2]
 print(f"Trying to connect to {ip_address}")
@@ -16,8 +27,15 @@ read_tickers()
 
 start_time = time.time()
 
+reqs = []
 for i in range(100):
     c = rpyc.connect(ip_address, port=18861)
-    print(c.root.get_stock_price(tickers[i]))
+    async_request = rpyc.async_(c.root.get_stock_price)
+    res = async_request(tickers[i])
+    res.add_callback(print_result)
+    reqs.append(res)
+    # print(res.value)
 
+while not check_reqs(reqs):
+    pass
 print(f"took {time.time() - start_time} seconds")
